4d59630d0694b753bdab1cb2d3fcc8644df30207
RobertaCavallaro <roberta.pianist@gmail.com>
2022-03-09 20:32:05 -0800

    initial commit

---
+++ js/01-coinflip-redux.js
@@ -0,0 +1,151 @@
+//  var coinFlip
+
+//  for (var coinFlip = 1; coinFlip <= 10; coinFlip++) {
+//   document.write (`you are on item ${coinFlip}<br>`)
+//  };
+
+
+// for(  i=1; i<11; i++){ 
+//   var coinFlip = Math.floor(2 * Math.random()) ;
+//   if (coinFlip == 0)
+//   { 
+//     window.console.log ("heads " + coinFlip  ); 
+//   } 
+//   else 
+//   { 
+//       window.console.log( "tails " + coinFlip );
+//    }
+//   }
+
+
+
+// var coinFlip
+// var i=1
+// do {
+//     i = i+2
+//      var coinFlip = Math.floor(2 * Math.random()) ;
+//      if (coinFlip == 0)
+//      { 
+//        window.console.log ("heads " + coinFlip  ); 
+//      } 
+//      else 
+//      { 
+//          window.console.log( "tails " + coinFlip );
+//       }
+//    } while (i < 11);
+
+
+
+
+
+// let name
+// for (let name = 1; name < 5; name++) {
+// window.console.log( "roberta" + name);}
+
+
+// let name = 1
+// do {
+//   window.console.log ('roberta' + name)
+//  name = name + 1
+// } while (name < 11);
+
+
+
+
+
+//triangle
+// for (var result = "#"; result.length <=7; result = result + "#")
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+// let coinFlip
+// do {
+// let coinFlip = Math.floor(2 * Math.random());
+// if (coinFlip == 0){
+//   window.console.log ('Heads');
+// }else{
+//   window.console.log ('Tails');
+//   break;
+// }
+// }
+//  while (coinFlip == 1)
+
+
+
+
+
+
+
+
+// even and odd
+// for (var x=0; x<=15; x++) {
+//   if (x === 0) {
+//           window.console.log(x +  " is even");
+//   }
+//   else if (x % 2 === 0) {
+//           window.console.log(x + " is even");   
+//   }
+//   else {
+//           window.console.log(x + " is odd");
+//   }
+// }
+
+
+
+
+
+//marco polo
+// for ( let i = 1; i <= 100; i++ )
+// {
+//  if ( i%3 === 0 && i%5 === 0)
+//  {
+//  console.log(`${i} Marco! Polo!`);
+//  } 
+//  else if ( i%3 === 0)
+//  {
+//  console.log (`${i} Marco!`);
+//  } else if ( i%5 === 0) 
+// {
+// console.log (`${i} Polo!`);
+// }
+// else{
+// console.log(i);
+// }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var countdown = 11;
+
+// while (countdown > 0){
+//     countdown--;
+//     console.log(countdown);
+// }
+// console.log(countdown);
+
+
+
+let num = 10
+while (num < 0){
+  num--;
+console.log (num)
+}
+console.log(num)
